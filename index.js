8576759361d72a5632e224a4f735741287d8a32d
Gaspar Saldana Sandoval <gaspar.saldana.sandoval@gmail.com>
2022-10-28 11:28:01 +0200

    regular expression to replace characters

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 import fs from "fs";
-console.log(fs);
+
+const folderContents = fs.readdirSync("back_to_the_future_dutch");
+console.log(folderContents);
+
+let allContent = "";
+//use for loop to go trough the data of the array
+for (let i = 0; i < folderContents.length; i++) {
+  allContent += fs.readFileSync(
+    "back_to_the_future_dutch/" + folderContents[i]
+  );
+}
+allContent = allContent.replace(/[^a-z]/gi, " "); //alle niet letters vervangen door een spatie
+allContent = allContent.replace(/ +/g, " ");
+console.log(allContent);
